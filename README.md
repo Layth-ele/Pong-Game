ea103ce3ad1f453276d19f0d771e47e5cf174ff3
Layth Al_Shblawi <layuth-ele@Layths-MacBook-Air.local>
2016-12-06 12:08:22 -0800

    republish pong project

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pong Game
+
+This is a basic pong game that used HTML5 Canvas. The purpose of this project is to explore the ES6 by using Object Oriented Programming (OOP).
+
+#Technologies Used
+
+* HTML5
+* CSS3
+* JavaScript ES6
+* webpack
+
+
+## Keys
+
+##### Player 1:
+* a: up
+* z: down
+
+##### Player 2:
+* ▲ : up
+* ▼: down
---
+++ src/paddle.js
@@ -0,0 +1,43 @@
+const victorySound = new Audio('../sounds/pong-02.wav');
+
+export default class Paddle {
+    constructor(height, x, control, score) {
+        this.width = 5;
+        this.height = 35;
+        this.x = x;
+        this.y = (height / 2) - (this.height / 2);
+        this.speed = 10;
+        this.maxHeight = height;
+        this.score = score;
+
+        document.addEventListener('keydown', event => {
+            switch (event.keyCode) {
+                case control.up:
+                    this.y = Math.max(
+                        0,
+                        this.y - this.speed
+                    );
+                    break;
+                case control.down:
+                    this.y = Math.min(
+                        this.maxHeight - this.height,
+                        this.y + this.speed
+                    );
+                    break;
+            }
+        });
+    }
+    addScore(player1, player2) {
+        this.score.score++;
+        victorySound.play();
+    }
+
+    render(ctx) {
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(
+            this.x, this.y,
+            this.width, this.height
+        );
+
+    }
+}
